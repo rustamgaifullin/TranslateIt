f7f003927b8eeb4ed85b59846517c797af50a26a
Rustam Gaifullin <rustam.gaifullin@gmail.com>
2016-08-27 22:40:14 +0200

    add searching during typing a text.

--- app/src/main/kotlin/com/rm/translateit/ui/activities/MainActivity.kt
+++ app/src/main/kotlin/com/rm/translateit/ui/activities/MainActivity.kt
@@ -7,11 +7,14 @@ import android.widget.EditText
 import android.widget.Spinner
 import android.widget.TextView
 import butterknife.bindView
+import com.jakewharton.rxbinding.view.RxView
+import com.jakewharton.rxbinding.widget.RxTextView
 import com.rm.translateit.R
 import com.rm.translateit.api.Translater
 import com.rm.translateit.api.TranslaterContext
 import com.rm.translateit.api.models.Language
 import com.rm.translateit.ui.adapters.LanguageSpinnerAdapter
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
@@ -36,14 +39,24 @@ class MainActivity : AppCompatActivity() {
         fromAdapter.updateLanguages(context.languages())
         toAdapter.updateLanguages(context.languages())
 
-        translateButton.setOnClickListener {
-            val word = wordEditText.text.toString()
-            val from = (fromSpinner.selectedItem as Language).code
-            val to = (toSpinner.selectedItem as Language).code
-            context.translate(word, from, to).subscribe({
-                resultQuery ->
-                    resultTextView.text = resultQuery
-            })
-        }
+        RxView.clicks(translateButton)
+                .subscribe( { search() } )
+
+        RxTextView.textChanges(wordEditText)
+                .throttleWithTimeout(500, TimeUnit.MILLISECONDS)
+                .subscribe({ search() })
     }
+
+    private fun search() {
+        val word = wordEditText.text.toString()
+        val from = (fromSpinner.selectedItem as Language).code
+        val to = (toSpinner.selectedItem as Language).code
+        context.translate(word, from, to).subscribe({
+            resultQuery ->
+            resultTextView.text = resultQuery
+        })
+    }
+
+
+
 }
