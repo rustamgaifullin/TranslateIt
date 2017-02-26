d77025ad214e9ca08b5f03cccd270db721a86241
Rustam Gaifullin <rustam.gaifullin@gmail.com>
2017-02-26 10:43:00 +0100

    add test for checking destination spinner's behaviour.

--- app/src/androidTest/java/com/rm/translateit/ui/activities/MainActivityTest.kt
+++ app/src/androidTest/java/com/rm/translateit/ui/activities/MainActivityTest.kt
@@ -4,10 +4,8 @@ import android.support.test.filters.LargeTest
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import com.rm.translateit.R
-import com.rm.translateit.utils.checkSpinnerEqualsToText
-import com.rm.translateit.utils.clickOnButton
-import com.rm.translateit.utils.clickOnSpinner
-import com.rm.translateit.utils.selectTextInSpinner
+import com.rm.translateit.api.translation.Services
+import com.rm.translateit.utils.*
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -21,7 +19,7 @@ open class MainActivityTest {
     var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
 
     @Test
-    fun testChangeLanguageButton() {
+    fun checkLanguageButtonSwapLanguagesProperly() {
         val originLanguage = "English"
         val destinationLanguage = "Polish"
 
@@ -35,5 +33,20 @@ open class MainActivityTest {
 
         checkSpinnerEqualsToText(R.id.origin_spinner, destinationLanguage)
         checkSpinnerEqualsToText(R.id.destination_spinner, originLanguage)
+    }
+
+    @Test
+    fun checkDestinationSpinnerShowsCorrectLanguages() {
+        val listOfLanguages = Services.languageService().languages().map { it.name }
+
+        listOfLanguages.forEach {
+            val originLanguage = it
+            clickOnSpinner(R.id.origin_spinner)
+            selectTextInSpinner(originLanguage)
+
+            val expectedDestinationLanguages = listOfLanguages.filter { it != originLanguage }
+            checkTextInSpinner(R.id.destination_spinner, expectedDestinationLanguages)
+            checkTextNotInSpinner(R.id.destination_spinner, originLanguage)
+        }
     }
 }
--- app/src/androidTest/java/com/rm/translateit/utils/ActivityUtils.kt
+++ app/src/androidTest/java/com/rm/translateit/utils/ActivityUtils.kt
@@ -1,15 +1,17 @@
 package com.rm.translateit.utils
 
-import android.support.test.espresso.Espresso
-import android.support.test.espresso.action.ViewActions
-import android.support.test.espresso.assertion.ViewAssertions
-import android.support.test.espresso.matcher.ViewMatchers
+import android.support.test.espresso.Espresso.onData
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.*
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.*
 import android.view.View
 import android.view.ViewGroup
 import com.rm.translateit.R
 import org.hamcrest.Description
 import org.hamcrest.Matcher
 import org.hamcrest.Matchers
+import org.hamcrest.Matchers.allOf
 import org.hamcrest.TypeSafeMatcher
 
 fun selectLanguages(originLanguage: String, destinationLanguage: String) {
@@ -21,51 +23,63 @@ fun selectLanguages(originLanguage: String, destinationLanguage: String) {
 }
 
 fun checkResult(title: String, translation: String) {
-    Espresso.onView(ViewMatchers.withId(R.id.result_recyclerView))
-            .check(ViewAssertions.matches(ViewMatchers.hasDescendant(ViewMatchers.withText(title))))
-    Espresso.onView(ViewMatchers.withId(R.id.result_recyclerView))
-            .check(ViewAssertions.matches(ViewMatchers.hasDescendant(ViewMatchers.withText(translation))))
+    onView(withId(R.id.result_recyclerView))
+            .check(matches(hasDescendant(withText(title))))
+    onView(withId(R.id.result_recyclerView))
+            .check(matches(hasDescendant(withText(translation))))
+}
+
+fun checkTextInSpinner(spinnerId: Int, texts: List<String>) {
+    for (text in texts) {
+        onData(withText(text))
+                .inAdapterView(withId(spinnerId))
+    }
+}
+
+fun checkTextNotInSpinner(spinnerId: Int, exceptText: String) {
+    onData(Matchers.not(withText(exceptText)))
+            .inAdapterView(withId(spinnerId))
 }
 
 fun clickOnSpinner(spinnerId: Int) {
-    val originSpinner = Espresso.onView(
-            Matchers.allOf(ViewMatchers.withId(spinnerId),
-                    ViewMatchers.withParent(Matchers.allOf(ViewMatchers.withId(R.id.activity_main),
-                            ViewMatchers.withParent(ViewMatchers.withId(android.R.id.content)))),
-                    ViewMatchers.isDisplayed()))
-    originSpinner.perform(ViewActions.click())
+    val originSpinner = onView(
+            allOf(withId(spinnerId),
+                    withParent(allOf(withId(R.id.activity_main),
+                            withParent(withId(android.R.id.content)))),
+                    isDisplayed()))
+    originSpinner.perform(click())
 }
 
 fun selectTextInSpinner(text: String) {
-    val originSpinnerTextView = Espresso.onView(
-            Matchers.allOf(ViewMatchers.withId(android.R.id.text1), ViewMatchers.withText(text), ViewMatchers.isDisplayed()))
-    originSpinnerTextView.perform(ViewActions.click())
+    val originSpinnerTextView = onView(
+            allOf(withId(android.R.id.text1), withText(text), isDisplayed()))
+    originSpinnerTextView.perform(click())
 }
 
 fun clickOnButton(buttonId: Int) {
-    val changeLanguageButton = Espresso.onView(
-            Matchers.allOf(ViewMatchers.withId(buttonId),
-                    ViewMatchers.withParent(Matchers.allOf(ViewMatchers.withId(R.id.activity_main),
-                            ViewMatchers.withParent(ViewMatchers.withId(android.R.id.content)))),
-                    ViewMatchers.isDisplayed()))
-    changeLanguageButton.perform(ViewActions.click())
+    val changeLanguageButton = onView(
+            allOf(withId(buttonId),
+                    withParent(allOf(withId(R.id.activity_main),
+                            withParent(withId(android.R.id.content)))),
+                    isDisplayed()))
+    changeLanguageButton.perform(click())
 }
 
 fun checkSpinnerEqualsToText(spinnerId: Int, text: String) {
-    Espresso.onView(Matchers.allOf(ViewMatchers.withId(android.R.id.text1),
-            childAtPosition(ViewMatchers.withId(spinnerId), 0),
-            ViewMatchers.isDisplayed()))
-            .check(ViewAssertions.matches(ViewMatchers.withText(text)))
+    onView(allOf(withId(android.R.id.text1),
+            childAtPosition(withId(spinnerId), 0),
+            isDisplayed()))
+            .check(matches(withText(text)))
 }
 
 fun typeTextAndPressEnter(text: String) {
-    val appCompatEditText = Espresso.onView(
-            Matchers.allOf(ViewMatchers.withId(R.id.word_editText),
-                    ViewMatchers.withParent(Matchers.allOf(ViewMatchers.withId(R.id.activity_main),
-                            ViewMatchers.withParent(ViewMatchers.withId(android.R.id.content)))),
-                    ViewMatchers.isDisplayed()))
-    appCompatEditText.perform(ViewActions.replaceText(text), ViewActions.closeSoftKeyboard())
-    appCompatEditText.perform(ViewActions.pressImeActionButton())
+    val appCompatEditText = onView(
+            allOf(withId(R.id.word_editText),
+                    withParent(allOf(withId(R.id.activity_main),
+                            withParent(withId(android.R.id.content)))),
+                    isDisplayed()))
+    appCompatEditText.perform(replaceText(text), closeSoftKeyboard())
+    appCompatEditText.perform(pressImeActionButton())
 }
 
 private fun childAtPosition(
