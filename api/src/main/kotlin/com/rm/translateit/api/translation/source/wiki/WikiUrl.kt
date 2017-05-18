6ab1675a6f153102f6d0d3ccd3c3abd385810d47
rustamgaifullin <rustam.gaifullin@gmail.com>
2017-05-18 16:56:20 +0200

    prepare a new api url for getting more details from wikipedia.

--- api/src/main/kotlin/com/rm/translateit/api/translation/source/wiki/WikiUrl.kt
+++ api/src/main/kotlin/com/rm/translateit/api/translation/source/wiki/WikiUrl.kt
@@ -4,12 +4,15 @@ import com.rm.translateit.api.models.LanguageModel
 import com.rm.translateit.api.translation.source.Url
 
 internal class WikiUrl: Url {
-    private val fullUrl = "https://%s.wikipedia.org/w/api.php?action=query&prop=langlinks&format=json&lllimit=500&redirects&lllang=%s&titles=%s"
+    private val searchUrl = "https://%s.wikipedia.org/w/api.php?action=query&prop=langlinks&format=json&lllimit=500&redirects&lllang=%s&titles=%s"
+    private val detailsUrl = "https://%s.wikipedia.org/w/api.php?action=query&format=json&prop=extracts|info&exintro=&explaintext=&inprop=url&titles=&s"
     
     override fun construct(word: String, from: LanguageModel, to: LanguageModel): String {
         val fromCode = from.code.toLowerCase()
         val toCode = to.code.toLowerCase()
 
-        return fullUrl.format(fromCode, toCode, word)
+        return searchUrl.format(fromCode, toCode, word)
     }
+
+    fun detailsUrl(word: String, to: LanguageModel): String = detailsUrl.format(to.code.toLowerCase(), word)
 }
