2df2963709e61dad36bca64ecd8d8ee49296d167
Rustam Gaifullin <rustam.gaifullin@gmail.com>
2018-10-12 23:17:34 +0200

    removed circleci builds so far

--- .circleci/deploy_dev.sh
+++
@@ -1,6 +0,0 @@
-curl \
--F "status=2" \
--F "notify=2" \
--F "ipa=@app/build/outputs/apk/debug/app-debug.apk" \
--H "X-HockeyAppToken: $HockeyAppToken" \
-https://rink.hockeyapp.net/api/2/apps/$AndroidAppId_Dev/app_versions/upload
