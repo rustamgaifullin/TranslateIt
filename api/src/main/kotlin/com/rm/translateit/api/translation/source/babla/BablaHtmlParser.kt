3f2d65368a4e2aed5cd09938f5fada69949948a8
Rustam Gaifullin <rustam.gaifullin@gmail.com>
2018-10-18 22:23:29 +0200

    fix small issues related to babla

--- api/src/main/kotlin/com/rm/translateit/api/translation/source/babla/BablaHtmlParser.kt
+++ api/src/main/kotlin/com/rm/translateit/api/translation/source/babla/BablaHtmlParser.kt
@@ -9,7 +9,7 @@ import org.jsoup.Jsoup
 internal class BablaHtmlParser: HtmlParser {
     override fun getTranslateItemsFrom(htmlString: String): Words {
         val document = Jsoup.parse(htmlString)
-        val resultElements = document.select("div.content:not(#similarWords) div.quick-results div.quick-result-overview ul.sense-group-results li a")
+        val resultElements = document.select("div.content:not(#similarWords):not(#compoundphrases) div.quick-results div.quick-result-overview ul.sense-group-results li a")
 
         val listOfWords = resultElements
                 .map { element -> element.text() }
@@ -22,13 +22,13 @@ internal class BablaHtmlParser: HtmlParser {
         val document = Jsoup.parse(htmlString)
         val resultElements = document.select("div.content:not(#similarWords) div.result-block.container div.sense-group div.dict-entry div.dict-translation")
 
-        val detailsResult = resultElements.joinToString("") { element ->
+        val detailsResult = resultElements.joinToString("\n") { element ->
             val original = element.select("div.dict-source strong").text()
             val alternative = element.select("div.dict-source span").text()
             val translation = element.select("div.dict-result a[href] strong").text()
             val type = element.select("div.dict-result span.suffix").text()
 
-            "$original$alternative: $translation$type\n"
+            "$original$alternative: $translation$type"
         }
 
         return Details(detailsResult, "")
